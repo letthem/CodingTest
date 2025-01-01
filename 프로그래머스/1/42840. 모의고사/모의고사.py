7beca272ffbeb588c334e67d28c8b88ee6a2c4e2
letthem <suyeon3662@naver.com>
2025-01-01 17:22:22 +0900

    [level 1] Title: 모의고사, Time: 2.52 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,29 +1,27 @@
 def solution(answers):
-
+    answer = []
+    
     length = len(answers)
     
-    # 1번 수포자
-    a = [(i % 5) + 1 for i in range(length)]
+    a = [1, 2, 3, 4, 5]
+    b = [2, 1, 2, 3, 2, 4, 2, 5]
+    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    # 2번 수포자
-    b_pattern = [2, 1, 2, 3, 2, 4, 2, 5] # 2번 수포자의 반복되는 패턴 정의
-    b = [b_pattern[i % len(b_pattern)] for i in range(length)]
+    # a, b, c의 득점
+    scores = [0] * 3
     
-    # 3번 수포자
-    c_pattern = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] # 3번 수포자의 반복되는 패턴 정의
-    c = [c_pattern[i % len(c_pattern)] for i in range(length)]
+    for i in range(length):
+        if (a[i % 5]) == answers[i]:
+            scores[0] += 1
+        if (b[i % 8]) == answers[i]:
+            scores[1] += 1
+        if (c[i % 10]) == answers[i]:
+            scores[2] += 1
     
-    # 맞춘 개수 count_a, count_b, count_c에 할당
-    counts = [
-        sum(1 for i in range(length) if answers[i] == person[i])
-        for person in [a, b, c]
-    ]
-    count_a, count_b, count_c = counts
+    max_value = max(scores)
     
-    # 가장 높은 점수
-    max_value = max(counts)
+    for i in range(len(scores)):
+        if scores[i] == max_value:
+            answer.append(i + 1)
     
-    # 동점자 처리 (1, 2, 3으로 오름차순 정렬)
-    answer = [i + 1 for i, count in enumerate(counts) if count == max_value]
-
     return answer
