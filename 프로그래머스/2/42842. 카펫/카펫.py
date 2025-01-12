bcaed96f01da9a20eaea8eee7df282213c1122d9
letthem <suyeon3662@naver.com>
2025-01-13 00:24:19 +0900

    [level 2] Title: 카펫, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,34 @@
+def solution(brown, yellow):
+    arr = []
+    
+    # x * 2 + y * 2 - 4 = brown
+    # (x - 2) * (y - 2) = yellow
+    
+    temp = int((brown + 4) / 2 - 2)
+    
+    # 초기화
+    temp2 = 0
+    y = 2
+    
+    #temp 끼리 같아질 때까지 돌면서 y값 찾기
+    while temp2 != temp:
+        y = y + 1 # y값 증가
+        temp2 = ( yellow / ( y - 2 )) + y
+        
+    # y값으로 x값 찾기
+    x = int(( brown + 4 ) / 2 - y)
+    
+    arr.append(x)
+    arr.append(y)
+    
+    return arr
+
+    
+    
+    # if (arr[0] < arr[1]) :
+    #     temp = arr[0]
+    #     arr[0] = arr[1]
+    #     arr[1] = temp
+
+    # return (arr)
+    return
